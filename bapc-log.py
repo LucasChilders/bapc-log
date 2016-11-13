f9bcebc426b7c9b7460abdc514e3325bd2ac6a61
LucasChilders <LucasChilders>
2016-11-13 01:49:57 -0800

    Master

---
+++ bapc-log.py
@@ -0,0 +1,48 @@
+import os
+import praw
+import csv
+import datetime
+import re
+
+def getPost(sub):
+    reddit = praw.Reddit(user_agent = "/u/LucasChilders")
+    post = reddit.get_subreddit(sub).get_new(limit = 1)
+    post = list(post)
+    return post[0]
+
+def getDate(post):
+    time = post.created
+    return datetime.datetime.fromtimestamp(time)
+
+def writeFile(post):
+    logFile = open("log.csv", "a", newline="")
+    writer = csv.writer(logFile, delimiter = ',')
+
+    title = post.title
+    title = title.replace(",", "|")
+
+    catagory = title.split("]")[0]
+    catagory = catagory.split("[")[1]
+
+    if catagory.lower() == "meta":
+        logFile.close()
+        print("META post found, returning.")
+        return
+
+    title = title.split("]")[1]
+    title = title.strip()
+
+    price = title.split("$")[1]
+    price = price.split(" ")[0]
+    price = re.findall(r'\d+', price)[0]
+
+    data = catagory + "," + title + "," + price + "," + str(getDate(post)) + "," + str(post.url)
+    data = data.split(",")
+
+    writer.writerow(data)
+
+    logFile.close()
+
+
+post = getPost("buildapcsales")
+writeFile(post)
